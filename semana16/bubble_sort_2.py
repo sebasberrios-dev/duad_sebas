b68f9284ec7720d5debf5c4d7f89147da6855f89
berrios0804 <berriossebas92@gmail.com>
2025-05-25 00:23:39 -0600

    Ejercicios Semana 16

---
+++ semana16/bubble_sort_2.py
@@ -0,0 +1,41 @@
+def bubble_sort(list_to_sort):
+    if not isinstance(list_to_sort, list):
+        raise TypeError('Error: la entrada debe ser una lista.')
+
+    if not list_to_sort:
+        print('La lista está vacía. No se puede ordenar')
+        return False
+    
+    print(f'Lista inicial: {list_to_sort}')
+    list_length = len(list_to_sort)
+
+    for outer_index in range(list_length):
+        has_made_changes = False
+        print(f'\n--- Pasada {outer_index + 1} ---')
+
+        for index in range(list_length - 1 - outer_index):
+            current_element = list_to_sort[index]
+            next_element = list_to_sort[index + 1]
+
+            print(f'- Comparando {current_element} y {next_element} en posiciones {index} y {index + 1}')
+
+            if current_element > next_element:
+                print('  Intercambiando...')
+                list_to_sort[index], list_to_sort[index + 1] = next_element, current_element
+                has_made_changes = True
+            else:
+                print('  No se intercambia.')
+
+        print(f'Lista después de la pasada {outer_index + 1}: {list_to_sort}')
+
+        if not has_made_changes:
+            print('La lista ya está ordenada. Terminando antes.')
+            break
+
+    return list_to_sort
+
+try:
+    my_list = 'hola'
+    bubble_sort(my_list)
+except TypeError as e:
+    print(e)
